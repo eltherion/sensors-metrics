669cd149b96b9a66a516324051827f3840e5c791
mmurawski <Mateusz Murawski>
2019-04-07 14:10:57 +0200

    Tests passing for row mapper implementation.

--- src/main/scala/com/datart/sensors/metrics/mapping/RowMapper.scala
+++ src/main/scala/com/datart/sensors/metrics/mapping/RowMapper.scala
@@ -1,5 +1,6 @@
 package com.datart.sensors.metrics.mapping
 
+import com.datart.sensors.metrics.model.Row._
 import com.datart.sensors.metrics.model._
 
 trait RowMapper {
@@ -7,5 +8,27 @@ trait RowMapper {
 }
 
 class RowMapperImpl extends RowMapper {
-  override def toRow(rawRow: String): Row = ???
+
+  private val failedMeasureR = ".*?(\\S+).*?,.*?(?:NaN).*?".r
+  private val measureR       = ".*?(\\S+).*?,.*?(-?\\d+).*?".r
+
+  private val minHumidity        = 0
+  private val maxHumidity        = 100
+  private val validHumidityRange = Range(minHumidity, maxHumidity)
+
+  override def toRow(rawRow: String): Row = {
+    rawRow match {
+      case measureR(sensorId, humidity) =>
+        val h = humidity.toInt
+        if (validHumidityRange.contains(h)) {
+          SuccessfulMeasurement(Sensor(sensorId.trim), Humidity(h))
+        } else {
+          Ignored
+        }
+      case failedMeasureR(sensorId) =>
+        FailedMeasurement(Sensor(sensorId.trim))
+      case _ =>
+        Ignored
+    }
+  }
 }
